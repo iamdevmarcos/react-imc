606df9ac7cf7f1cc11789e037713bef8e6ae4676
Marcos Andre <marcosdev.me@gmail.com>
2022-02-24 16:29:56 -0300

    feat: add styles

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -24,10 +24,45 @@ export const RightSide = styled.div`
   flex: 1;
 `;
 
-export const Title = styled.h1``;
+export const Title = styled.h1`
+  margin: 0;
+  font-size: 40px;
+  color: #3a4b5c;
+`;
 
-export const Description = styled.p``;
+export const Description = styled.p`
+  font-size: 16px;
+  margin-bottom: 40px;
+  color: #6a7d8b;
+`;
 
-export const Input = styled.input``;
+export const Input = styled.input`
+  width: 100%;
+  border: 0;
+  outline: 0;
+  border-bottom: 2px solid rgba(150, 163, 171, 0.5);
+  padding: 10px 2px;
+  margin-bottom: 20px;
+  font-size: 14px;
 
-export const Button = styled.button``;
+  &:focus {
+    border-bottom: 2px solid rgba(150, 163, 171, 1);
+  }
+`;
+
+export const Button = styled.button`
+  background: #227c9d;
+  color: #fff;
+  font-size: 15px;
+  border: 0;
+  border-radius: 10px;
+  padding: 15px 0;
+  width: 100%;
+  cursor: pointer;
+  margin-top: 40px;
+  transition: all ease 0.3s;
+
+  &:hover {
+    opacity: 0.8;
+  }
+`;

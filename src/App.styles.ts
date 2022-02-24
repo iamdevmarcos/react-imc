bf86f09bae50f57528e8693186f485ada4f64dd6
Marcos Andre <marcosdev.me@gmail.com>
2022-02-24 16:03:38 -0300

    feat: add initial structure

---
+++ src/App.styles.ts
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const Main = styled.div``;
+export const Header = styled.div``;
+
+export const Area = styled.div`
+  max-width: 900px;
+  margin: 40px auto;
+`;
+
+export const Logo = styled.img``;
+
+export const Container = styled.div`
+  display: flex;
+  gap: 80px;
+  max-width: 900px;
+  margin: auto;
+`;
+
+export const LeftSide = styled.div`
+  flex: 1;
+`;
+export const RightSide = styled.div`
+  flex: 1;
+`;

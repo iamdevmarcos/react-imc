dea440083649755b8359a11352ce811e63c53659
Marcos Andre <marcosdev.me@gmail.com>
2022-02-25 08:44:58 -0300

    style: add styles to griditem

--- src/components/GridItem/index.tsx
+++ src/components/GridItem/index.tsx
@@ -1,14 +1,30 @@
 import { Level } from "../../types/Level";
 import * as C from "./styles";
 
+import upImage from "../../assets/up.png";
+import downImage from "../../assets/down.png";
+
 type Props = {
   data: Level;
 };
 
 export const GridItem = ({ data }: Props) => {
   return (
-    <C.Container>
-      <h1>{data.title}</h1>
+    <C.Container bgColor={data.color}>
+      <C.Icon>
+        <C.Image
+          src={data.icon === "up" ? upImage : downImage}
+          alt=""
+          width="30"
+        />
+      </C.Icon>
+      <C.Title>{data.title}</C.Title>
+      <C.Info>
+        <>
+          IMC está entre <strong>{data.imc[0]}</strong> e{" "}
+          <strong>{data.imc[1]}</strong>
+        </>
+      </C.Info>
     </C.Container>
   );
 };
--- src/components/GridItem/styles.ts
+++ src/components/GridItem/styles.ts
@@ -1,3 +1,36 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div<{ bgColor: string }>`
+  background: ${(props) => props.bgColor};
+  flex: 1;
+  color: #fff;
+  border-radius: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  padding: 20px;
+`;
+
+export const Icon = styled.div`
+  width: 70px;
+  height: 70px;
+  border-radius: 50%;
+  background: rgba(0, 0, 0, 0.1);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Image = styled.img``;
+
+export const Title = styled.div`
+  font-size: 20px;
+  font-weight: bold;
+  margin-top: 5px;
+`;
+
+export const Info = styled.div`
+  font-size: 12px;
+  margin-top: 14px;
+`;
